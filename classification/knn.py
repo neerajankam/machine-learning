682bc7a202a7fa6a3ecc84f5fe0a5ce987b3b5dc
Neeraj Ankam <ankam.neeraj@gmail.com>
2023-07-16 16:49:05 -0400

    Refactor code

--- classification/knn.py
+++ classification/knn.py
@@ -7,42 +7,63 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 
-# Load data
-kaggle.api.authenticate()
-kaggle.api.dataset_download_files(
-    "dawerty/cleaned-daytrading-training-data", path="data/", unzip=True
-)
-df = pd.read_csv("data/stock_trading.csv")
 
-# Separate features and target
-X = df.drop("is_profit", axis=1)
-y = df["is_profit"]
+class KNN:
+    dataset_path = None
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
-)
+    @classmethod
+    def run(cls):
+        cls.authenticate()
+        cls.download_dataset()
+        cls.predict()
 
-# Feature selection
-from sklearn.feature_selection import SelectKBest, mutual_info_classif
+    @classmethod
+    def authenticate(cls):
+        kaggle.api.authenticate()
 
-selector = SelectKBest(mutual_info_classif, k=18)
-selector.fit_transform(X_train.drop(["sym", "datetime"], axis=1), y_train)
+    @classmethod
+    def download_dataset(cls):
+        kaggle.api.dataset_download_files(
+            "dawerty/cleaned-daytrading-training-data", path="data/", unzip=True
+        )
 
-# Scale features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train.drop(["sym", "datetime"], axis=1))
-X_test = scaler.transform(X_test.drop(["sym", "datetime"], axis=1))
+    @classmethod
+    def predict(cls):
+        df = pd.read_csv("data/train.csv")
 
-# KNN model
-knn = KNeighborsClassifier(n_neighbors=7)
+        # Separate features and target
+        X = df.drop("is_profit", axis=1)
+        y = df["is_profit"]
 
-# Fit model
-knn.fit(X_train, y_train)
+        # Split data
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.3, random_state=42
+        )
 
-# Predict on test data
-y_pred = knn.predict(X_test)
+        # Feature selection
+        from sklearn.feature_selection import SelectKBest, mutual_info_classif
 
-# Evaluate accuracy
-acc = knn.score(X_test, y_test)
-print(acc)
+        selector = SelectKBest(mutual_info_classif, k=18)
+        selector.fit_transform(X_train.drop(["sym", "datetime"], axis=1), y_train)
+
+        # Scale features
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train.drop(["sym", "datetime"], axis=1))
+        X_test = scaler.transform(X_test.drop(["sym", "datetime"], axis=1))
+
+        # KNN model
+        knn = KNeighborsClassifier(n_neighbors=7)
+
+        # Fit model
+        knn.fit(X_train, y_train)
+
+        # Predict on test data
+        y_pred = knn.predict(X_test)
+
+        # Evaluate accuracy
+        acc = knn.score(X_test, y_test)
+        print(acc)
+
+
+if __name__ == "__main__":
+    KNN().run()
